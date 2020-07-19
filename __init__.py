8b519158f865b948130046be766b6c51354599b0
Faskomilo <oscarve.do@outlook.com>
2020-07-18 23:56:43 -0500

    url handler working fine

---
+++ __init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/<action>/<controller>", methods=["GET","POST"])
+def listen(action, controller):
+    if os.path.isfile("../views/" + action + "_" + controller + ".html"):
+        return render_template("../views/" + action + "_" + controller + ".html"), 200
+    else:
+        return not_found_error(404)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    app.run()
