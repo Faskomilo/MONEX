c351480651bce651a151047b8b6fb875cd6617df
OMEGONCIO <jozue25200al@gmail.com>
2020-07-18 23:24:01 -0500

    added ForeignKeys to entities

--- models/voiceBills.py
+++ models/voiceBills.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
 
 engine = create_engine(
     'mysql+mysqlconnector://<user>:<password>@localhost:3306/<default_db>...')
@@ -11,7 +11,7 @@ class User(Base):
 
      id = Column(Integer, primary_key=True, nullable=False)
      recording = Column(String, nullable=False)
-     idBill = Column(Integer, nullable=False)
+     idBill = Column(Integer,ForeignKey("bills.d"), nullable=False)
 
      def __repr__(self):
         return "<User(recording='%s', idBill='%s')>" % (
