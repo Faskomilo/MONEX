c439f4eb31607bcb684f3f3cb806710faa3fa0ed
OMEGONCIO <jozue25200al@gmail.com>
2020-07-20 03:30:35 -0500

    AJAX code added to indexJS, html page redirect uploaded

---
+++ static/JS/admin_login.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('#goToIndex').click(function(){
+      var origin   = window.location.origin;
+      window.location.replace(origin + "/MONEX/index");
+    });
+  });
+  
+  function mute(){
+    $('#botMute').show();
+    $('#botUnmute').hide();
+  }
+  
+  function unmute(){
+    $('#botMute').hide();
+    $('#botUnmute').show();
+  }
+  
+  function continueLog(){
+    if($('#inputUser').val() == ''){
+      $('#errorUser').show();
+    }
+    else{
+      $('#errorUser').hide();
+    }
+    if($('#inputPasword').val() == ''){
+      $('#errorPasword').show();
+    }
+    else{
+      $('#errorPasword').hide();
+    }
+  }
+  
